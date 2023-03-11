6250844f97fdfc89aaf94d34c98693fb0f13b9cc
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2023-03-11 19:21:28 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './app.css'
+import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Components/Home/Home"
+import Footer from "./Components/Footer/Footer"
+import Main from "./Components/Main/Main"
 const App = () => {
   return (
-    <h2>App</h2>
+    <>
+    <Navbar/>
+    {/* <Home/>
+    <Main/>
+    <Footer/> */}
+    </>
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+    <Section></Section>
+    </div>
+  )
+}
+
+export default Navbar
