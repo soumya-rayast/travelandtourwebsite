41c9b26504cb3cd2842063684b764cc475f1de15
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2023-03-12 08:53:11 +0530

    Updated navbar css file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,13 +8,14 @@ const Navbar = () => {
     <div>
     <section classnName="navbarSection">
       <header className="header flex">
-        <div className="logo">
+        <div className="logoDiv">
           <a href="#" className="logo flex">
             <h1> <MdModeOfTravel className='icon'/>ghumoPhiro.</h1>
           </a>
         </div>
         <div className="navBar">
           <ul className="navLists flex">
+            
             <li className="navItem">
               <a href="#" className="navLink">Home</a>
             </li>
@@ -40,6 +41,7 @@ const Navbar = () => {
               <a href="#">Book Now</a>
             </button>
           </ul>
+
           <div className="closeNavbar">
             <AiFillCloseCircle  className='icon'/>
           </div>
