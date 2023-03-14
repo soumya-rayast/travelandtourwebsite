8b5e5a89b1f3ef278fd0564690c48a2e67624ac4
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2023-03-14 23:46:01 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <Navbar/>
     <Home/>
     <Main/>
-    {/* <Footer/> */}
+    <Footer/>
     </>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import "./footer.css"
+import video2 from '../../Media/video2.mp4'
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='footer'>
+      <div className="videoDiv">
+      {/* <video src={video2} muted autoPlay loop type="video/mp4" ></video> */}
+      <video loop muted autoPlay>
+        <source src={video2} type="video/mp4"/>
+      </video>
+      </div>
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>Keep in Touch</small>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
